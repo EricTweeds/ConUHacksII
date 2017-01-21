cf1e65049c12045baab6aafd4161501feef3309b
seanlee98 <sangyoon_0203@hotmail.com>
2017-01-21 15:16:10 -0500

    implemented word count and testing filter

--- app/src/main/java/waterwoo/conuhacks/WordFilter.java
+++ app/src/main/java/waterwoo/conuhacks/WordFilter.java
@@ -1,19 +1,39 @@
 package waterwoo.conuhacks;
 
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Sean on 1/21/2017.
  */
 
-public class WordFilter extends MainActivity{
-    public static void main(String[] args) {
-        List<String> fillerWords = Arrays.asList("um", "uhm", "uh");
-        String speech = "Hi, my name is Sean uh, I like uhm dogs";
-        for (String fillerWord : fillerWords) {
-            speech = speech.replaceAll("(?i)\\b[^\\w -]*" + fillerWord + "[^\\w -]*\\b", "");
+public class WordFilter extends WordCounter{
+    public static void main(String[] args) throws IOException {
+        FileReader file = new FileReader("C:/Users/Sean/Documents/ConuHacks2017/conuhacks/ConUHacksII/app/src/main/test.txt");
+        BufferedReader reader = new BufferedReader(file);
+
+        List<String> speech = new ArrayList<String>();
+        String line = reader.readLine();
+
+        //while loop so we can go through every line in the text and pass it through our string
+        while(line != null){
+            speech.add(line);
+            line = reader.readLine();
         }
+        //testing to make sure string is outputted
         System.out.println(speech);
+
+        String filter = "uhm";
+        List<String> filteredSpeech = new ArrayList<String>();
+
+        for(int i = 0; i < speech.size(); i++){
+            if(speech.get(i).contains(filter)){
+                filteredSpeech.add(speech.get(i));
+                System.out.println("You said " + filter + " this many times: " + filteredSpeech.size());
+            }
+        }
     }
 }
